236ee12db61237cd2f0acb6f29b0d2622d19c8ca
Jennifer Stephens <xxjennandtonicxx@gmail.com>
2022-08-05 16:56:46 -0700

    added image and Description header

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ header.setAttribute('class', 'dog-name')
 header.append('Rizzo')
 content.append(header)
 
+let dogContent = document.createElement('div')
+dogContent.setAttribute('class', 'dog-content')
+content.append(dogContent)
+
+let dogImage = document.createElement('img')
+dogImage.setAttribute('class', 'dog-image')
+dogImage.setAttribute('src', './assets/rizzo.jpg')
+content.append(dogImage)
+
+let dogDetails = document.createElement('div')
+dogDetails.setAttribute('class', 'dog-details')
+content.append(dogDetails)
+let header2 = document.createElement('h3')
+header2.setAttribute('class', 'header-2')
+header2.append("Description:")
+dogDetails.append(header2)
+
+
+
+
+
 
 
 
